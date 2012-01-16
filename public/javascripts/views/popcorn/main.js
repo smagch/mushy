38f083194b56b3997a1e791666cdcd5f97efbfbe
smagch <smagch@gmail.com>
2012-01-17 08:45:38 +0900

    change style name

--- public/javascripts/views/popcorn/main.js
+++ public/javascripts/views/popcorn/main.js
@@ -7,7 +7,11 @@ require.config({
       , 'popcorn' : 'libs/popcorn/main'
     }
 });
-
+// TODO 1. build just output text ui 
+// TODO 2. and then build make them span tool
+// TODO 3. and then build glue and sissor tool
+// TODO 4. and then make them work with popcorn
+// TODO 5. build slider and so on
 require(['jQuery', 'underscore', 'backbone', 'popcorn'], function ($, _, Backbone, Popcorn) {
     console.log('loaded');
     
