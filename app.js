60a5f516b731805b3c040d29a96513e337f408fd
smagch <smagch@gmail.com>
2012-01-16 09:09:54 +0900

    scaffold app

---
+++ app.js
@@ -0,0 +1,56 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , stylus = require('stylus')
+  ;
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'hoge' }));
+  //app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(stylus.middleware({ 
+       src: __dirname
+     , dest: __dirname + '/public'
+     , compile: compile
+  }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+function compile(str, path) {
+  return stylus(str)
+    .import(__dirname + '/stylesheets/mixins')
+    .set('filename', path)
+    .set('warn', true)
+    .set('compress', true);
+}
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+
+
+app.get('/', function (req, res) {
+   console.log('hoge'); 
+   res.render('index', {
+      title : 'home' 
+   });
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
