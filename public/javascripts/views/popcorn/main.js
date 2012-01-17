7f3c86a051ce22ba6ee7376dd77421d9dbe28d81
smagch <smagch@gmail.com>
2012-01-17 11:01:49 +0900

    be able to output text

--- public/javascripts/views/popcorn/main.js
+++ public/javascripts/views/popcorn/main.js
@@ -13,8 +13,50 @@ require.config({
 // TODO 4. and then make them work with popcorn
 // TODO 5. build slider and so on
 require(['jQuery', 'underscore', 'backbone', 'popcorn'], function ($, _, Backbone, Popcorn) {
-    console.log('loaded');
+console.log('loaded');
+$(document).ready(function () {
     
+    var $editingSpan;
     
+    $('#text-input').on('keydown', function (e) {
+        console.log('keydown');
+        if(e.keyCode === 13) {
+            console.log('enter');            
+            if($editingSpan) {
+                $editingSpan
+                  .removeClass('editing')
+                  .text($(this).val());
+                $editingSpan = undefined;
+            } else {
+                var text = '<span>' + $(this).val() + '</span>';
+                $('#text-container').append(text);
+            }            
+            $(this).val('');            
+        }
+    });         
+    /// how to deal with 
+    $('#text-container').on('click', 'span', function (e) {        
+        $editingSpan = $(this);
+        $('#text-input').focus().val(          
+          $(this)
+            .addClass('editing')            
+            .text()
+        );
+        e.stopPropagation();
+    });
+    
+    // reset functions
+    $(document).on('mouseup', function (e) {        
+        if($editingSpan && !$(e.target).is('input') ) {
+            console.log('document click');        
+            $editingSpan.removeClass('editing');
+            $('#text-input').val('');
+            $editingSpan = undefined;
+        }
+        e.stopPropagation();
+    });
+        
+        
+});// document ready
 });
 
