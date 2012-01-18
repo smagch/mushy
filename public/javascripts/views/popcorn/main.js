657993cb39b0f280b6efc31c3490593da0e636a1
smagch <smagch@gmail.com>
2012-01-18 19:11:25 +0900

    make text input realtime

--- public/javascripts/views/popcorn/main.js
+++ public/javascripts/views/popcorn/main.js
@@ -8,15 +8,22 @@ require.config({
     }
 });
 // make real time?
-// TODO 3. and then build glue and sissor tool
-// TODO 4. and then make them work with popcorn
-// TODO 5. build slider and so on
+// TODO - and then build glue and sissor tool
+// TODO - and then make them work with popcorn
+
+
+// TODO - select span when youtube slider is moving, ask.
+// TODO - make time-input correct using Popcorn util
+// TODO - use effect? 
+// TODO - make this plugin becasuse onStart and onEnd isn't necessary with each trackEvent 
+//if I make this plugin removing jquery, ...
+
 require(['jQuery', 'underscore', 'backbone', 'popcorn'], function ($, _, Backbone, Popcorn) {
 console.log('loaded');
 $(document).ready(function () {
     
     var $editingSpan;
-    
+
     $('#text-input').on('keydown', function (e) {
         console.log('keydown');
         var val = $(this).val();
@@ -25,16 +32,41 @@ $(document).ready(function () {
             if($editingSpan) {
                 endSelectSpan();
             } else {
-                var text = '<span>' + val + '</span>';
-                $('#text-container').append(text);
+                //var text = '<span>' + val + '</span>';
+                //$('#text-container').append(text);
             }            
             $(this).val('');
             e.stopPropagation();
         }        
-    });         
-    /// how to deal with 
+    });
+    $('#text-input').on('focusin', function (e) {
+        console.log('focusin');
+        $(this).on('keyup', updateText);
+    });
+    
+    $('#text-input').on('focusout', function (e) {
+        console.log('focusout');
+        $(this).off('keyup', updateText);
+    });
+    
+    function updateText(e) {
+        if(!$editingSpan) {
+            var val = $(this).val();
+            if(val === '') {
+                return;
+            }
+            var text = '<span>' + val + '</span>';
+            //$editingSpan = $(text).appendTo('#text-container');
+            selectSpan.call($(text).appendTo('#text-container'));
+            return;
+            //$('#text-container').append(text);            
+        }
+        $editingSpan.text($(this).val());
+    }
+
     $('#text-container').on('click', 'span', function (e) {
         selectSpan.call(this);
+        youtube.pause();
         e.stopPropagation();
     });
     
@@ -81,10 +113,11 @@ $(document).ready(function () {
           ;
         $('#time-input-start').val(start);
         $('#time-input-end').val(end);
-        // if(start !=) {
-        //             youtube.currentTime(start);
-        //         }
         
+        var id = $editingSpan.attr('data-id');
+        if(youtube.getTrackEvent(id)) {
+            youtube.currentTime(start);
+        }        
     }
     // this span
     function deselectSpan() {
@@ -137,13 +170,7 @@ $(document).ready(function () {
           , end = $el.attr('data-end')
           ;
       
-        // youtube.footnote({
-        //            start : start
-        //          , end : end
-        //          , text : $el.text()
-        //          , target : 'footnote'
-        //        })
-        var id = Math.random().toString(16).substr(2);
+        var id = Popcorn.guid();
         $el.attr('data-id', id);
         
         youtube.code({
@@ -151,25 +178,56 @@ $(document).ready(function () {
           , end : end
           , id : id
           , onStart : function (options) {
-                console.log('onstart');                
+                //console.log('onstart');                
                 //selectSpan.call(el);
                 var elem = $('#text-container > span[data-id=' + this._id + ']')[0]
                 selectSpan.call(elem);
             }
           , onEnd : function (options) {
-                console.log('onend');
+                //console.log('onend');
                 deselectSpan();
             }
         });
         
-        
+ 
         // youtube.listen('trackend', function (e) {
         //            console.log('trackend');
         //            
         //        });
     });
+    youtube.listen('playing', function (e) {
+        console.log('playing');
+    });
     
+    youtube.listen('play', function (e) {
+        console.log('play');
+    });
+    
+    youtube.listen('waiting', function (e) {
+        console.log('wating');
+    });
+    youtube.listen('abort', function (e) {
+        console.log('abort');
+    });
+    
+    youtube.listen('ratechange', function (e) {
+        console.log('ratechange');        
+    });
+    youtube.listen('ended', function (e) {
+        console.log('ended');
+    });
+    
+    youtube.listen('seeked', function (e) {
+         console.log('----seeked----');
+    });
+     
+    youtube.listen('seeking', function (e) {
+         console.log('++++seeking++++');
+    });
+     
     youtube.play();
+    
+    // for easy debug
     window.youtube = youtube;
     window.$ = $;
     // $('#ajust').on('click', function (e) {
