f11c8627b0e888161e382197bd632e0fcb8dff77
smagch <smagch@gmail.com>
2012-01-20 16:53:02 +0900

    add teardown and arrange anonymouse function

--- public/javascripts/views/popcorn/main.js
+++ public/javascripts/views/popcorn/main.js
@@ -38,6 +38,102 @@ console.log('loaded');
 (function(Popcorn){
 	Popcorn.plugin('transcript', (function(){
 	  
+		// Register container by id. value is just 1
+		// Allow more than one transcript container
+		var containers = { };
+
+		function registerContainer(options) {
+			var self = this,
+					containerStr = options.target,
+					container = document.getElementById(containerStr),
+					spans = container.getElementsByTagName('span');
+					container.setAttribute('data-id', options._id);					
+			
+			
+			// Gives keyboard focus
+			if(container.tabIndex === -1) {
+				container.tabIndex = 10;
+			}
+			
+			// 
+			Popcorn.forEach(spans , function (span, index) {				  
+				var id = span.id = Popcorn.guid('transcript'),
+            start = span.getAttribute('data-start'),
+				    end = span.getAttribute('data-end');
+
+ 				Popcorn.addTrackEvent(self, {
+ 					start : start || -1,
+ 				  end : end || -1,
+ 				  id : id,
+ 				  _running : false,
+ 				  compose : [],
+ 				  effect : [],
+					target : containerStr,
+ 				  _natives : options._natives
+ 				});
+			});
+			
+			var _clickHandler = clickHandler.bind(container, this),
+					_keydownHandler = keydownHandler.bind(container, this);
+			
+			container.addEventListener('click', _clickHandler, false);			
+			container.addEventListener('keydown', _keydownHandler, false);
+			// register container
+			containers[options._id] = function () {
+				container.removeEventListener('click', _clickHandler, false);			
+				container.removeEventListener('keydown', _keydownHandler, false);
+			}
+		}
+		
+		// Delegate event to span element
+		function clickHandler(player, e) {
+			console.log('clicked');
+		  if( e.target.tagName !== 'SPAN' || !e.target.id) {
+		    return;
+		  }
+		  console.log('has id');
+		  var trackEvent = player.getTrackEvent(e.target.id);//Popcorn.getTrackEvent.ref(player, e.target.id);
+		  if(!trackEvent) {
+		    return;
+		  }
+		  console.log('has trackEvent');
+
+		  var start = trackEvent.start;
+		  console.log('start : ' + start);				  
+
+			if(isValidTime(player, start)) {						
+				player.currentTime(start);
+			} else {
+				player.trigger('notimeselect', trackEvent );
+			}
+		}
+		
+		function keydownHandler(player, e) {
+			// Get currently selected span          
+		  var currentSpans = this.getElementsByClassName('editing');
+       if(!currentSpans.length) {
+         return;
+       }          
+       var nextSpan = {
+           '40' : currentSpans[0].nextElementSibling,    // down                
+           '38' : currentSpans[0].previousElementSibling // up
+       }[e.keyCode];
+
+       if(!nextSpan || !nextSpan.id) {
+         return;
+       }          
+       var nextTrack = player.getTrackEvent(nextSpan.id);   
+       if( nextTrack ) {
+        var start = nextTrack.start;
+
+				if(!isValidTime(player, start)) {						
+					player.trigger('notimeselect', nextTrack );
+				} else {
+					player.currentTime(nextTrack.start);
+				}       
+       }
+		}
+		
 		function selectById(id) {
 			var elem = document.getElementById(id);
 			elem && elem.classList.add('editing');
@@ -51,89 +147,24 @@ console.log('loaded');
 		function isValidTime(player, time) {
 			return !(time < 0 || time > player.duration() );
 		}
-
+						
 		return {
 			_setup : function (options) {
 				console.log('========_setup=======');				 
-				if(!options.target && !options.container) {
+				if(!options.target ) { // && !options.container) {
 					throw new Error('target is not defined');
 					return;
 				}
-				var self = this,
-					containerStr = options.container,
-					container = document.getElementById(containerStr),
-					spans = container.getElementsByTagName('span');
-					
-				// Gives keyboard focus
-				console.log('container.tabIndex : ' + container.tabIndex);				
-				container.tabIndex = 10;
-
-				Popcorn.forEach(spans , function (span, index) {				  
-					var id = span.id = Popcorn.guid('transcript'),
-              start = span.getAttribute('data-start'),
-					    end = span.getAttribute('data-end');
-					    			 
-  				Popcorn.addTrackEvent(self, {
-  					start : start || -1,
-  				  end : end || -1,
-  				  id : id,
-  				  _running : false,
-  				  compose : [],
-  				  container: containerStr,
-  				  effect : [],
-  				  target : false,
-  				  _natives : options._natives
-  				});
-				});
-				
-				// Delegate event to span element
-				container.addEventListener('click', function (e) {
-				  console.log('clicked');
-				  if( e.target.tagName !== 'SPAN' || !e.target.id) {
-				    return;
-				  }
-				  console.log('has id');
-				  var trackEvent = Popcorn.getTrackEvent.ref(self, e.target.id);
-				  if(!trackEvent) {
-				    return;
-				  }
-				  console.log('has trackEvent');
-				  
-				  var start = trackEvent.start;
-				  console.log('start : ' + start);				  
-				  self.currentTime(start);
-					if(!isValidTime(self, start)) {						
-						self.trigger('notimeselect', trackEvent );
-					}
-				}, false);
-				
-				container.addEventListener('keydown', function (e) {
-				  // Get currently selected span          
-				  var currentSpans = this.getElementsByClassName('editing');
-          if(!currentSpans.length) {
-            return;
-          }          
-          var nextSpan = {
-              '40' : currentSpans[0].nextElementSibling,    // down                
-              '38' : currentSpans[0].previousElementSibling // up
-          }[e.keyCode];
-
-          if(!nextSpan || !nextSpan.id) {
-            return;
-          }          
-          var nextTrack = self.getTrackEvent(nextSpan.id);   
-          if( nextTrack ) {
-	          var start = nextTrack.start;
-						self.currentTime(nextTrack.start);
-						if(!isValidTime(self, start)) {						
-							self.trigger('notimeselect', nextTrack );
-						}       
-          }          
-				}, false);									 
+								
+				if(!containers[options._id]) {
+					// Remove trackEvent of container
+					// this.removeTrackEvent(options._id);
+					registerContainer.call(this, options);
+				}				
 			},
-			_teardown	: function () {
-			  // TODO - remove Events
-			  
+			_teardown	: function (options) {
+				container[options._id].call();
+			  delete container[options._id];
 			},
 			start : function (event, options) {
 				console.log('-----------------start-----------------');
@@ -157,29 +188,37 @@ $(document).ready(function () {
     Popcorn.plugin.debug = true;
     var youtube = Popcorn.youtube( '#video', 'http://www.youtube.com/watch?v=CxvgCLgwdNk' );  
     youtube.transcript({
-        container : 'text-container'   
+        //container : 'text-container'   
+			target : 'text-container'
     });
     
     youtube.listen('trackend', function (e) {
       console.log('trackend');
 			// TODO - make sure if there is no running track for span,
 			$('#text-input').val('');
+			currentTrack = undefined;
     }); 
-		var callbacks = [ ];
+		var callbacks = [ ],
+				currentTrack;
+		
 		
     youtube.listen('trackchange', function (e) {
       console.log('trackchange');
 			fillInputs(e);
+			currentTrack = e;			
     });
 		
 		youtube.listen('notimeselect', function (e) {			
 			console.log('notimeselect');			
 			fillInputs(e);
+			currentTrack = e;
 			$('#'+ e._id).addClass('editing');
 			callbacks.push(function () {
 				$('#'+e._id).removeClass('editing');
+				currentTrack = undefined;
 			});
 		});
+		
 		function fillInputs(e) {
 			while(callbacks.length) {
 				var fn = callbacks.shift();
@@ -189,6 +228,67 @@ $(document).ready(function () {
       $('#time-input-start').val(e.start);
 			$('#time-input-end').val(e.end);
 		}
+		
+		$('#text-input').on('focusin', function (e) {
+        console.log('focusin');
+				// find currently selected span
+				
+        $(this).on('keyup', updateText);
+    });
+    
+    $('#text-input').on('focusout', function (e) {
+        console.log('focusout');
+        $(this).off('keyup', updateText);
+    });
+    
+    function updateText(e) {
+			if(currentTrack) {
+				var id = currentTrack._id;
+				$('#' + id).text($('#text-input').val());
+			} else {
+				var val = $('#text-input').val();
+        if(val === '') {
+        	return;
+        }				
+				currentTrack = insertSpan(val);
+      }
+    }
+		
+		// retern trackEvent
+		function insertSpan(val) {
+			var id = Popcorn.guid('transcript'),
+					text = '<span id=' + id + '>' + val + '</span>',
+					start = $('#time-input-start').val(),
+					end = $('#time-input-end').val();					
+			
+			(start === '') && (start = -1);
+			(end === '') && (end = -1);
+			
+			youtube.transcript({
+				start : start,
+				end : end,
+				id : id,
+				_running : false,
+				target : 'text-container'
+				
+	//			_natives : youtube.transcript
+			});
+			// Popcorn.addTrackEvent(self, {
+			// 		start : start || -1,
+			// 	  end : end || -1,
+			// 	  id : id,
+			// 	  _running : false,
+			// 	  compose : [],
+			// 	  container: containerStr,
+			// 	  effect : [],
+			// 	  target : false,
+			// 	  _natives : options._natives
+			// 	});
+			$('#text-container').append(text);
+			return youtube.getTrackEvent(id);
+		}
+
+
     youtube.listen('playing', function (e) {
         console.log('playing');
     });
@@ -207,6 +307,7 @@ $(document).ready(function () {
     youtube.listen('ratechange', function (e) {
         console.log('ratechange');        
     });
+
     youtube.listen('ended', function (e) {
         console.log('ended');
     });
