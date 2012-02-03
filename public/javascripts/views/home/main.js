ae6c70136615d4495edc9e004e12f516a643a62a
smagch <smagch@gmail.com>
2012-02-04 08:31:01 +0900

    add left column

--- public/javascripts/views/home/main.js
+++ public/javascripts/views/home/main.js
@@ -13,7 +13,10 @@
     // TODO - design tab
     // TODO - make them draggable
     // TODO - try sortable
-    // TODO - datepicker
+    // TODO - datepicker - topsy
+    // TODO - load entity image
+    // TODO - add 'load before' and 'load after' buttons, with id search
+    // TODO - create left column, look at backbone opensource example
     
     var noop = function() {};
     // templates
@@ -29,9 +32,15 @@
       
         var M = { };
         
-        M.TwitterModel = Backbone.Model.extend({});        
-        M.TwitterImageModel = Backbone.Model.extend({});
-        M.FacebookModel = Backbone.Model.extend({});
+        M.TwitterModel = Backbone.Model.extend({
+          type : 'twitter'
+        });        
+        M.TwitterImageModel = Backbone.Model.extend({
+          type : 'image'
+        });
+        M.FacebookModel = Backbone.Model.extend({
+          type : 'facebook'
+        });
         
         return M;
     })();
@@ -263,14 +272,16 @@
                     this.template({
                         model : this.model.toJSON()
                     })
-                );
+                )
+                .attr('id', this.model.cid );
+                
                 return this;
             }
         });        
         
         V.Item['twitter-search'] = V.ItemBaseView.extend({
             template : Templates['twitter-template']
-          , className : 'clearfix tweet'
+          , className : 'clearfix tweet '
         });
         
         V.Item['twitter-user'] = V.Item['twitter-id'] = V.Item['twitter-search'];
@@ -299,6 +310,7 @@
                 this.collection
                 .on('reset', this.render, this )
                 .on('add', this.addItem, this )
+                .on('remove', this.removeItem, this )
                 .on('requeststart', this.showLoding, this )
                 .on('requestend', this.hideLoding, this );
             }
@@ -318,6 +330,14 @@
                 } else {
                     this.createItem( data );
                 }
+            }
+          , removeItem : function( model ) {
+                console.log('remove');
+                var id = model.cid;
+                this.$('li[id='+ id + ']').remove();
+                
+                
+                
             }                  
           , render : function () {
                 
@@ -343,8 +363,11 @@
             el : '#search-content'
           , currentKey : undefined
           , currentView : undefined
+          , views : { }
+          , currentCollection : function() {
+                return this.currentView.collection;
+            }
           , initialize : function() {
-                this.views = { };               
                 this.changeView('twitter-search');
                 var $el = this.$el
                   , el = this.el;
@@ -359,13 +382,13 @@
                 }
             }
           , loadNext : function() {
-                var collection = this.currentView.collection;
+                var collection = this.currentCollection();
                 if( collection.hasNext() ) {
                     collection.queryNext();
                 }
             }
           , doQuery : function( queryString ) {
-                var collection = this.currentView.collection;
+                var collection = this.currentCollection();
                 collection.query( queryString );
             }
           , changeCategory : function( secoundKey ) {
@@ -402,59 +425,334 @@
         return V;
     })();
     
+    
+    (function(M, C, V){
+        
+        M['MashUpModel'] = Backbone.Model.extend({
+            types : 'twitter facebook text'.split(/\s/g)
+        });
+        
+        C['MashUpCollection'] = Backbone.Collection.extend({
+            model : M.MashUpModel          
+        });
+        
+        V.ArticleItem = { };
+        
+        V.ArticleItem['twitter'] = V.ItemBaseView.extend({
+            template : Templates['twitter-template']
+          , className : 'clearfix tweet'
+        });
+        
+        V.ArticleItem['facebook'] = Backbone.View.extend({
+          
+        });
+        
+        V.ArticleItem['text'] = Backbone.View.extend({
+            
+        });
+                        
+        V.ArticleView = Backbone.View.extend({
+            el : '#article'
+          , initialize : function() {
+                this.collection = new C['MashUpCollection']();
+                this.collection.on('add', this.addItem, this );
+                this.collection.on('reset', this.render, this );
+                this.collection.on('change', this.change, this );
+            }
+          , events : {
+            
+            }
+          , change : function(e) {
+                console.log('change');
+                console.dir( e );
+            }
+          , addItem : function( model ) {
+                console.log('add item article');
+                // model.type
+                console.log('model.type : ' + model.type);
+                var targetView = V.ArticleItem[model.type];
+                if( !targetView ) {
+                    throw new Error('model type : ' + model.type + ' is invalid');
+                }
+                var view = new targetView({ model : model });
+                this.$el.append(view.render().el);
+            }
+          , render : function() {
+                console.log('render article');
+            }
+        });
+        
+      
+    })(Models, Collections, Views);
+
+    (function($, undefined){
+    		
+        var isDirty = false
+    			, offsetX
+    			, offsetY
+    			, $el
+    			, isEnter = false
+    			, selector
+    			, $dropTargets
+    			, requestAnimFrame = (function(){			
+    				return window.requestAnimationFrame || 
+    						window.mozRequestAnimationFrame ||
+    						window.oRequestAnimationFrame ||
+    						window.msRequestAnimationFrame ||
+    						function(callback) {
+    								window.setTimeout(callback, 1000 / 60);
+    						};
+    		})();
+
+        function mouseMoveHandler(e) {
+    				if(isDirty) {
+    						return;
+    				}
+    				
+    				isDirty = true;
+    				var x = offsetX + e.clientX
+    					, y = offsetY + e.clientY;
+
+    				requestAnimFrame(function() {
+    						$el.offset({
+    								top : y
+    							, left : x
+    						});
+    						isDirty = false;
+    				});								
+        }
+        
+        
+        function mouseUpHandler(e) {
+    			$(document)
+    				.off( 'mousemove', selector, mouseMoveHandler )
+    				.off( 'mouseup', selector, mouseUpHandler );
+    			
+    			
+    			var centerX = e.clientX + $el.width() * 0.5;
+    			var right = $dropTargets.offset().left + $dropTargets.outerWidth();
+    			console.log('$dropTargets.offset().left : ' + $dropTargets.offset().left);
+    			
+    			
+    			console.dir( e );
+    			console.log('centerX : ' + centerX);
+    			console.log('right : ' + right);
+    			// TODO rewrite nicely
+    			if(centerX < right ) {
+    			    console.log('drop');
+    			    console.log('e.data.id : ' + e.data.id);        			    
+    			    $dropTargets.trigger('drop', e.data.id );    			            			    
+    			}        			        			        	
+    			
+    			$el.remove();
+    			$el = undefined;        			
+        }
+
+        function mouseDownHandler(e) {
+            //$el = $(this);
+            
+            var $self = $(this)
+              , offset = $self.offset();
+              
+            if( $el ) {
+              throw new Error('$el is not removed')
+              mouseUpHandler();
+            }
+            
+            $el = $self.clone().css({
+                left : offset.left
+              , top : offset.top
+              , position : 'absolute'
+              , width : $self.width()
+              , height : $self.height()
+            }).appendTo('body');
+            
+            offsetX = $el.offset().left - e.clientX
+        		offsetY = $el.offset().top - e.clientY;			
+        		selector = e.data.selector;
+        		
+        		var data = $.extend({
+        		    id : $el.attr('id')
+        		}, e.data);
+        		
+        		$el.attr('id', '');
+        		
+        		$(document)
+        		  .on( 'mousemove', data.selector, data, mouseMoveHandler )
+        		  .on( 'mouseup', data.selector, data, mouseUpHandler );
+        		
+        		// TODO use namespace??  
+        		if( $dropTargets ) {
+        		    $dropTargets
+        		               		    
+        		}
+        }                                  
+
+        $.fn['dragBox'] = function(options) {
+            var selector = options.target;
+            if( !_.isString(selector) ) {
+                throw new Error('target should be selector string');
+            }
+            var data = {
+                selector : selector              
+            };
+            $(this).on( 'mousedown', selector, data, mouseDownHandler );
+        }
+
+        $.fn['dropTarget'] = function(options) {
+            if( $dropTargets ) {
+                $dropTargets.add(this);
+            } else {
+                $dropTargets = $(this);
+            }
+            return this;
+            
+            
+            // var target = options.target;
+            //                 if( _.isString(target) ) {
+            //                     throw new Error('target should be string');
+            //                 }
+            //$(this).on( 'mouseover', mouseOverHandler );
+
+            //$(this).on('mousedown', target, mouseDownHandler );
+        }
+
+    })(jQuery);
+    (function(V){
+        
+        V.RightColumn = Backbone.View.extend({
+            el : '#right-column'
+          , currentCollection : function() {
+                return this.contentView.currentCollection();
+            }
+          , initialize : function () {
+                var content = this.contentView = new Views.ContentView();
+                this.$searchInput = $('#search-input');
+
+                $('#search-tab').tabs()
+                .on('tabsselect', function(e, ui) {
+                    var key = [ ];
+                    // TODO write more nicely
+                    key.push(ui.panel.id.split('-')[2]);        
+                    var secoundKey = $( ui.panel ).children('span.selected').text().replace(/\s+/g, '');
+                    if( secoundKey !== '' ) {
+                      key.push( secoundKey );
+                    }        
+                    content.changeView( key.join('-') );
+                })
+                .on('click', 'span.button', function(e) {
+                    console.log('span clicked');
+                    var category =  $( e.target )
+                        .addClass('selected')
+                        .siblings('.selected')
+                          .removeClass('selected')
+                          .end()
+                        .text().replace(/\s+/g, '');
+                    console.log('category : ' + category);            
+                    content.changeCategory( category );
+                });
+                                
+
+              	
+                content.on('viewchange', this.viewChangeHandler, this );
+            }        
+          , events : {
+                'keyup #search-input' : 'keyupHandler'                                       
+            }    
+          , keyupHandler : function( e ) {
+                if( e.keyCode === 13 ) {
+                    this.viewChangeHandler();
+                }
+            }
+          , viewChangeHandler : function() {
+                var val = this.$searchInput.val();
+                if( val !== '' ) {
+                    this.contentView.doQuery(val);
+                }
+            }
+        });
+        
+        V.LeftColumn = Backbone.View.extend({
+            el : '#article'
+          , initialize : function() {
+                //this.collection = new C['MashUpCollection']();
+                //this.collection.on('add', this.addItem, this );
+                this.articleView = new V.ArticleView();
+            }
+          , events : {
+            
+            }
+          , addModel : function( model ) {
+                this.articleView.collection.add( model );
+            }                
+        });
+
+        
+        
+        V.AppView = Backbone.View.extend({
+            el : '#content'
+          , initialize : function() {
+                this.leftColumn = new V.LeftColumn();
+                this.rightColumn = new V.RightColumn();
+                // TODO why container is draggable??
+                // $('#article-content').sortable({
+                //                    revert : true
+                //                })
+                //                .on('sortreceive', function(e) {
+                //                    console.log('recieve');
+                //                    console.dir( e );
+                //                    currentCollection.remove(draggingModel);                  
+                //                })
+                //                .on('sortcreate', function(e) {
+                //                    console.log('sort create');
+                //                    console.dir( e );
+                //                });
+              	//.disableSelection();
+              	var self = this;
+              	var draggingModel
+              	  , currentCollection;
+              	$('#search-content').dragBox({
+                	  target : 'li'
+                });
+                
+                $('#article').dropTarget({
+                    //target : ''
+                }).on('drop', _.bind( this.dropHandler, this) );            
+                
+              	// $('#search-content').on('mousedown', V.ItemBaseView.prototype.tagName , function(e) {
+              	//                   var id = $(this).draggable({
+              	//                       connectToSortable : '#article-content'
+              	//                     , helper : 'clone'
+              	//                     , addClasses : false
+              	//                     , appendTo : '#article-content'
+              	//                   }).attr('id');
+              	//                   console.log('id : ' + id);     
+              	//                   currentCollection = self.rightColumn.currentCollection();             
+              	//                   draggingModel = currentCollection.getByCid(id);
+              	//                   console.dir( draggingModel );
+              	//                   $(document).one('mouseup', $(this).destroy );
+              	//                   e.stopPropagation();
+              	//                 })
+              	
+            }
+          , events : {
+            
+            }
+          , dropHandler : function( e, id ) {
+                console.log('id : ' + id);
+                var currentCollection = this.rightColumn.currentCollection();
+                var model = currentCollection.getByCid(id);                
+                console.dir( model );
+                this.leftColumn.addModel(model);
+                currentCollection.remove( model );
+            }
+        });
+        
+        
+    })( Views );            
 
     
-    
-    
-
-    
-    Views.AppView = Backbone.View.extend({
-        el : $('#content')
-      , initialize : function () {
-            var content = this.contentView = new Views.ContentView();
-            this.$searchInput = $('#search-input');
-            
-            $('#search-tab').tabs()
-            .on('tabsselect', function(e, ui) {
-                var key = [ ];
-                // TODO write more nicely
-                key.push(ui.panel.id.split('-')[2]);        
-                var secoundKey = $( ui.panel ).children('span.selected').text().replace(/\s+/g, '');
-                if( secoundKey !== '' ) {
-                  key.push( secoundKey );
-                }        
-                content.changeView( key.join('-') );
-            })
-            .on('click', 'span.button', function(e) {
-                console.log('span clicked');
-                var category =  $( e.target )
-                    .addClass('selected')
-                    .siblings('.selected')
-                    .removeClass('selected')
-                    .end()
-                    .text().replace(/\s+/g, '');
-                console.log('category : ' + category);            
-                content.changeCategory( category );
-            });
-            
-            content.on('viewchange', this.viewChangeHandler, this );
-            
-        }        
-      , events : {
-            'keyup #search-input' : 'keyupHandler'
-        }
-      , keyupHandler : function( e ) {
-            if( e.keyCode === 13 ) {
-                this.viewChangeHandler();
-            }
-        }
-      , viewChangeHandler : function() {
-            var val = this.$searchInput.val();
-            if( val !== '' ) {
-                this.contentView.doQuery(val);
-            }
-        }
-    });
+  
     
     window.Collections = Collections;
     window.Views = Views;   
