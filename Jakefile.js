7fe846fc3d9f90545284d6b468a259b790773d03
smagch <smagch@gmail.com>
2012-02-17 03:52:04 +0900

    add build task

--- Jakefile.js
+++ Jakefile.js
@@ -1,9 +1,11 @@
 var 
   fs = require( "fs" ),
   _ = require( "underscore" ),
+  requirejs = require('requirejs'),
   jshint = require( "jshint" ).JSHINT,
   colors = require( "colors" ),
   uglifyjs = require( "uglify-js" ),
+  minimatch = require('minimatch'),
   Buffer = require( "buffer" ).Buffer,  
   dateFormat = require( "dateformat" ),
   cp = require("child_process"),
@@ -13,11 +15,33 @@ var
   child;
   
 var config = {
+  dist: {
+    libs: 'out/libs.js',
+    app: 'out/mushroom.js'
+  },
   src: [
     'public/javascripts/modules/*',
     'public/javascripts/main.js',
     'public/javascripts/sortive.js'
   ],
+  libs: {
+    include: [
+      'public/javascripts/libs/*',
+      'public/javascripts/sortive.js',
+    ],
+    // currently doesn't used
+    exclude: [
+      'public/javascripts/libs/jsonselect.js',
+      'public/javascripts/libs/modernizr*',
+      'public/javascripts/libs/popcorn*'
+    ],
+    // make sure plugin script to load after prioratized script
+    // default 0, using minimatch
+    priorities: {
+      'public/javascripts/libs/jquery-1*.js': 1,
+      'public/javascripts/libs/underscore*.js': 1
+    }
+  },
   //'dest': 'public/javascripts/MR.js',
   //'dest-min': 'public/javascripts/MR.min.js',  
   jshint: {
@@ -70,7 +94,7 @@ function hint( src, path ) {
   }
 }
 
-function readFile( filepath ) {
+function readFile(filepath) {
   var src;
   try {
     src = fs.readFileSync( filepath, "UTF-8" );
@@ -80,11 +104,112 @@ function readFile( filepath ) {
   }
 }
 
+function writeFile(filePath, contents) {
+  console.log('about to write into : ' + filePath.underline);
+  try {
+    fs.writeFileSync(filePath, contents, "UTF-8");
+  } catch( e ) {
+    fail( e );
+  }
+  console.log('ok');
+}
+
 
 desc('Start Test concat and hint');
 task('default', ['hint'], function() {
   console.log('--------------' + 'complete'.green.bold + '--------------' );
 });
+
+desc('test just a test');
+task('test', function() {
+  console.log('about to test');
+  var deps = [ 'hoge', 'foo', 'smagch', '33f', 'fadsf'];
+  var libs = ['jquery'];
+  var priorities = {
+    smagch: 1,
+    foo: 1
+  };
+  function comparator(val) {
+    return - priorities[val] || 0;
+  }
+  var files = new jake.FileList();
+  files.include(deps);
+  files.include(libs);
+  var arr = _.sortBy( files.toArray(), comparator );  
+  console.dir( arr );
+});
+
+
+
+
+
+// TODO add min files
+// concat libs
+// TODO how to make sure that css file exist? currently public/stylesheets is ignored.. should be tracked?
+// 1. output css from styl
+// 2. output html from jade
+// 3. concat minified libs
+// TODO 
+// 4. ugrify and concat modules reading config,
+// output file should contains 
+// * one html file
+// * one or two js file
+// * one css file
+// * images
+// last ordinary require call to optimized script and see if there is error
+namespace('build', function() {
+  desc('build module and libs');
+  task('all', ['build:module', 'build:libs'], function() {
+    console.log('complete'.yellow.underline.bold);
+  });
+  
+  desc('ugrify and concat modules');
+  task('module', function() {
+    console.log('about to build modules');
+    // TODO - add build date and license
+    requirejs.optimize({
+      baseUrl: __dirname + '/public/javascripts',
+      name: 'main',    
+      out: config.dist.app
+    }, function(res) {
+      console.log('------------modules build complete------------'.yellow.underline);
+    });
+  });
+  
+  desc('concat libs');
+  task('libs', function() {
+    console.log('about to concat libs...');
+    var libs = new jake.FileList();
+    libs.include(config.libs.include);
+    libs.exclude(config.libs.exclude);
+    
+    var priorities = config.libs.priorities,
+      arr = _.sortBy(libs.toArray(), function(path){
+        
+      for(var matcher in priorities) {
+        if(minimatch(path, matcher)) {
+          console.log('match : ' + path);
+          return -priorities[matcher];
+        }
+      }
+      return 0;
+    });
+
+    var body = '';
+    arr.forEach(function(path) {
+      console.log('opening : ' + path.underline);      
+      var content = readFile(path);
+      body += '\n' + content +'\n';
+    });
+    // after load require.js,, where to ....
+    writeFile(config.dist.libs, body);
+    console.log('---concat complete---'.yellow);
+  });
+});
+
+
+desc('build modules');
+task('')
 
 desc('hint');
 task('hint', function() {
@@ -97,4 +222,5 @@ task('hint', function() {
     hint(body, filePath);
   });
 });
+
 
